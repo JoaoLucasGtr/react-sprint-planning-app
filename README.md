470c226c9ef94196927a860529ededef3ca12019
João Lucas <joaolucasgtr@gmail.com>
2020-05-15 21:30:18 -0300

    Incluídos arquivos para transformar a aplicação em PWA;

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Sprint Planning Web App
+
+This is a simple project, created with the purpose of studying the PWA technology.
+
+The app itself is hosted by Github Pages in this [link](https://joaolucasgtr.github.io/sprint-planning-app/).
+
+## About
+
+This app is focused in the process of estimating tasks. You can create projects, add tasks to it and set story points for each one.
+
+All data will be stored in **localStorage**, so you can refresh or close the page without worries of losing your progress.
+
+With a minimalistic design, you'll be able to explore all resources with ease.
+
+Enjoy yourself.
+
+---
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const storage = new LocalStorage();
 
-document.addEventListener('DOMContentLoaded', feedback);
+document.addEventListener('DOMContentLoaded', () => {
+  registerSW();
+  feedback();
+});
 
 function addProject() {
   let element = document.getElementById('input-projects');
@@ -54,7 +57,7 @@ function feedback() {
        <span class="h5 text-info" style="cursor: pointer">Pontos: ${storage.calcTotalPoints()}</span>
      </div>`
   if (projects.length > 0) {
-    headerTemplate += `<button class="btn btn-outline-danger ml-auto" onclick="clearAll()">
+    headerTemplate += `<button class="btn btn-outline-danger ml-auto" onclick="clearAll()" title="Discard all">
       <i class="fa fa-trash"></i>
      </button>`;
   }
@@ -64,29 +67,29 @@ function feedback() {
   projects.forEach(p => {
     let tasks = p.tasks || [];
     projectsTemplates += `
-      <div class="card card-body shadow">
+      <div class="card card-body shadow" style="min-width: 30em;">
         <button class="close ml-auto" onclick="removeProject(${p.id})">
           <span aria-hidden="true">&times;</span>
         </button>
-        <div class="mb-3 d-flex align-items-center"> 
-          <span class="h4 font-weight-bold">${p.name}</span>          
+        <div class="mb-3 d-flex align-items-center">
+          <span class="h4 font-weight-bold">${p.name}</span>
           <div class="dropdown">
             <button class="btn btn-link dropdown-toggle" id="dropdownMenuButton-${p.id}" data-toggle="dropdown">
               <i class="fa fa-plus"></i>
             </button>
             <div class="dropdown-menu" aria-labelledby="dropdownMenuButton-${p.id}">
               <form class="p-3" style="width : 30em">
-                <label>Nova tarefa:</label>
-                <input id="input-task-proj-${p.id}" placeholder="Tarefa" class="form-control" />
-                <label>Pontos:</label>
-                <input id="input-pts-proj-${p.id}" placeholder="Pontos" class="form-control" />
-                <button onclick="addTask(${p.id})" class="btn btn-outline-primary">Add</button>
+                <label>New task:</label>
+                <input id="input-task-proj-${p.id}" placeholder="Task name" class="form-control" />
+                <label>Points:</label>
+                <input id="input-pts-proj-${p.id}" placeholder="Task weight" class="form-control" />
+                <button onclick="addTask(${p.id})" class="btn btn-outline-primary">Submit</button>
               </form>
             </div>
           </div>
           <div class="ml-auto">
-            <span title="Quantidade" class="h5 text-muted" style="cursor: pointer">${tasks.length}</span> /
-            <span title="Pontos" class="h5 text-info" style="cursor: pointer"> ${storage.calcPoints(p.id)}</span>
+            <span title="Count" class="h5 text-muted">${tasks.length}</span> /
+            <span title="Points" class="h5 text-info"> ${storage.calcPoints(p.id)}</span>
           </div>
         </div>
         <ul class="list-group mb-3">
@@ -94,21 +97,32 @@ function feedback() {
             `<li class="list-group-item d-flex align-items-center">${t.description}
                <button class="btn btn-link" onclick="removeTask(${p.id}, ${t.id})">
                  <i class="fa fa-minus"></i>
-               </button>               
-               <span class="badge badge-info badge-pill ml-auto" title="Clique para atualizar a pontuação" onclick="show('${p.id}-${t.id}-input-update-points')">
+               </button>
+               <span class="badge badge-info badge-pill ml-auto" title="Click to edit" onclick="show('${p.id}-${t.id}-input-update-points')">
                  ${t.points}
                </span>
-               <input id="${p.id}-${t.id}-input-update-points" 
+               <input id="${p.id}-${t.id}-input-update-points"
+                      type="number"
                       value="0"
                       class="ml-2 form-control collapse"
-                      style="width: 4em" 
+                      style="width: 4em"
                       onblur="updatePoints(${p.id}, ${t.id}, this)"></input>
              </li>`
           ).join('')}
-        </ul>        
+        </ul>
       </div>`;
   });
 
   document.getElementById('totals').innerHTML = headerTemplate;
   document.getElementById('projects').innerHTML = projectsTemplates;
+}
+
+async function registerSW() {
+  if ('serviceWorker' in navigator) {
+    try {
+      await navigator.serviceWorker.register('./sw.js');
+    } catch (e) {
+      console.log(`SW registration failed`);
+    }
+  }
 }
